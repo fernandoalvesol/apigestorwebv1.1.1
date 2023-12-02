78efb535589fd34f3bdf2798ce2c311a8157d420
fernando <nandofacusi@gmail.com>
2023-12-01 21:43:33 -0300

    reqf1122023

--- resources/views/Caixa/entradas.blade.php
+++ resources/views/Caixa/entradas.blade.php
@@ -8,23 +8,29 @@
                         d="M11.5 14.875H14.875M14.875 14.875H18.25M14.875 14.875V11.5M14.875 14.875V18.25M4 8.5H6.25C6.84674 8.5 7.41903 8.26295 7.84099 7.84099C8.26295 7.41903 8.5 6.84674 8.5 6.25V4C8.5 3.40326 8.26295 2.83097 7.84099 2.40901C7.41903 1.98705 6.84674 1.75 6.25 1.75H4C3.40326 1.75 2.83097 1.98705 2.40901 2.40901C1.98705 2.83097 1.75 3.40326 1.75 4V6.25C1.75 6.84674 1.98705 7.41903 2.40901 7.84099C2.83097 8.26295 3.40326 8.5 4 8.5ZM4 18.25H6.25C6.84674 18.25 7.41903 18.0129 7.84099 17.591C8.26295 17.169 8.5 16.5967 8.5 16V13.75C8.5 13.1533 8.26295 12.581 7.84099 12.159C7.41903 11.7371 6.84674 11.5 6.25 11.5H4C3.40326 11.5 2.83097 11.7371 2.40901 12.159C1.98705 12.581 1.75 13.1533 1.75 13.75V16C1.75 16.5967 1.98705 17.169 2.40901 17.591C2.83097 18.0129 3.40326 18.25 4 18.25ZM13.75 8.5H16C16.5967 8.5 17.169 8.26295 17.591 7.84099C18.0129 7.41903 18.25 6.84674 18.25 6.25V4C18.25 3.40326 18.0129 2.83097 17.591 2.40901C17.169 1.98705 16.5967 1.75 16 1.75H13.75C13.1533 1.75 12.581 1.98705 12.159 2.40901C11.7371 2.83097 11.5 3.40326 11.5 4V6.25C11.5 6.84674 11.7371 7.41903 12.159 7.84099C12.581 8.26295 13.1533 8.5 13.75 8.5Z"
                         stroke="#341008" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
-                CONTROLE DE ENTRADAS
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-md-6 titlefluxo">
+                            <p>CONTROLE DE ENTRADAS</p>
+                        </div>
+                    </div>
+                </div> 
             </span>
-            <div class="caixa">
+            <div class="caixaentradas">
                 <form action="{{ route('entrada.store') }}" class="form" method="POST">
-                    <div class="px-2 py-2 w-100 d-grid">                    
+                    <div class="">                    
                         @csrf
                         <div class="p-2 radius-4 border">
-                            <div class="   p-2 pt-0 radius-4">
+                            <div class="">
                                 <div class="rows center-middle">     
-                                    <div class="col-5">
-                                        <label class="text-label d-block text-branco">Descrição </label>
+                                    <div class="col-md-5">
+                                        <label class="text-label">Descrição </label>
                                         <input type="text" name="descricao" required
-                                            value="" class="form-campo">
+                                            value="" class="form-control">
                                     </div>
                                     <div class="col-3">
                                         <label class="text-label d-block text-branco">Escolha a Operação </label>
-                                        <select name="operacao" id="operacao" value="operacao" class="form-campo">
+                                        <select name="operacao" id="operacao" value="operacao" class="form-control">
                                             <option value="selecione">Selecione</option>
                                             <option value="MENSALIDADE">Mensalidade</option>
                                             <option value="ADESAO">Taxa de Adesão</option>
@@ -34,7 +40,7 @@
                                     </div>
                                     <div class="col-3">
                                         <label class="text-label d-block text-branco">Escolha o Escritorio </label>
-                                        <select name="escritorio" id="escritorio" value="escritorio" class="form-campo">
+                                        <select name="escritorio" id="escritorio" value="escritorio" class="form-control">
                                             <option value="selecione">Selecione</option>
                                             <option value="ITAQUITINGA">Setta Itaquitinga</option>
                                             <option value="CONDADO">Setta Condado</option>
@@ -50,7 +56,7 @@
                                     </div>
                                     <div class="col-3">
                                         <label class="text-label d-block text-branco">Forma de Pagamento </label>
-                                        <select name="pagamento" id="pagamento" value="pagamento" class="form-campo">
+                                        <select name="pagamento" id="pagamento" value="pagamento" class="form-control">
                                             <option value="selecione">Selecione</option>
                                             <option value="DINHEIRO">DINHEIRO</option>
                                             <option value="TRANSFERENCIA">TRANSFERÊNCIA</option>
@@ -62,12 +68,12 @@
                                         <label class="text-label d-block text-branco">Data Entrada </label>
                                         <input type="date" name="data" required
                                             value=""
-                                            class="form-campo">
+                                            class="form-control">
                                     </div>
                                     <div class="col-2">
                                         <label class="text-label d-block text-branco">Informe o Valor </label>
                                         <input type="text" name="preco" value=""
-                                            required class="form-campo">
+                                            required class="form-control">
                                     </div>
                                     <div class="col-1 mt-0 pt-5">
                                         <input type="submit" value="Enviar" class="w-100 btn btn-roxo text-uppercase">
--- storage/framework/views/c46fbf93a80cf818f5dc6c77e26ac6bb.php
+++ storage/framework/views/c46fbf93a80cf818f5dc6c77e26ac6bb.php
@@ -5,16 +5,20 @@
     <title>Sistema Comercial - mjailton</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale =1">
-    <!--css-->
+    <!--css
     <link rel="stylesheet" href="<?php echo e(asset('assets/componentes/css/style_Componente.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('assets/js/datatables/css/jquery.dataTables.min.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('assets/js/datatables/css/responsive.dataTables.min.css')); ?>">
-
-    <link rel="stylesheet" href="<?php echo e(asset('assets/css/auxiliar.css')); ?>">
+-->
+    <!--<link rel="stylesheet" href="<?php echo e(asset('assets/css/auxiliar.css')); ?>"> -->
     <link rel="stylesheet" href="<?php echo e(asset('assets/css/grade.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('assets/css/style.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/estilo.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('assets/css/home-venda.css')); ?>">
     <link rel="stylesheet" href="<?php echo e(asset('assets/css/style-m.css')); ?>">
+
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!--font icones-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
@@ -100,6 +104,10 @@
         <script type="text/javascript" src="<?php echo e(asset('assets/js/js_produto.js')); ?>"></script>
     <?php endif; ?>
 
+    <!--Bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
 </body>
